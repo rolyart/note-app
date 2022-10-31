0c96708d0ad945087fef18ad5c58293fc97abec2
Roland Maruntalu <rolandmaruntelu@gmail.com>
2022-10-31 14:22:44 +0200

    Update bug

--- assets/js/notes.js
+++ assets/js/notes.js
@@ -149,7 +149,10 @@ function drawNotes(){
                 noteFooter.innerHTML = '<time>'+dateFormat(new Date(el.date),df,'-')+'</time>';
 
                 let deleteNote = document.createElement('button');
-                deleteNote.innerHTML = '<i class="lni lni-trash-can"></i>';
+                deleteNote.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+              </svg>`;
 
                 deleteNote.addEventListener('click', ()=>{
                     storageData.deleteNote(el)
@@ -166,25 +169,29 @@ function drawNotes(){
                     <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                 </svg>`;
 
+                let noteTi = document.createElement('input');
+                let noteTe = document.createElement('textarea');
+
                 editNote.addEventListener('click', ()=>{
-                    //storageData.deleteNote(el)
-                    // .then(res=>{
-                    //     console.log('The note has been deleted.');
-                    //     notes.remove();
-                    //     drawNotes();
-                    // })
-                    let noteTi = document.createElement('input');
+                    
+
+
+                    note.classList.add('update');
                     noteTi.value = el.title;
                     noteTitle.innerHTML = '';
                     noteTitle.appendChild(noteTi);
 
-                    let noteTe = document.createElement('textarea');
+                  
                     noteTe.value = el.text;
                     noteTe.setAttribute('placeholder', 'Type note and press enter');
                     noteText.innerHTML = '';
                     noteText.appendChild(noteTe);
                     noteTe.focus();
+                    noteTe.style.height = 'auto';
+                    noteTe.style.height = noteTe.scrollHeight + 'px';
                     noteTe.addEventListener('keyup', ()=>{
+                        noteTe.style.height = 'auto';
+                        noteTe.style.height = noteTe.scrollHeight + 'px';
                         if (event.keyCode === 13) {
                             if(noteTe.value.length>800)  {
                                 alert('The note entered is too long.')
@@ -205,7 +212,22 @@ function drawNotes(){
                         }
                             
                     }, false);
+
+                    noteTe.addEventListener('blur', (event) => { 
+                        let data = localStorage.getItem('Notes')?JSON.parse(localStorage.getItem('Notes')):[]; 
+                        var index = data.findIndex(function(n){
+                            return n.date === el.date;
+                        })
+
+                        let note = { title:noteTi.value, text:noteTe.value, date:new Date(), color:el.color }
+                        data[index] = note;
+                        localStorage.setItem('Notes', JSON.stringify(data));
+                        document.getElementsByClassName('notes')[0].remove();
+                        drawNotes();
+                    });
+        
                 });
+
 
                 note.appendChild(noteTitle);
                 note.appendChild(noteText);
@@ -401,11 +423,10 @@ function NotesApp(appName, notesColors){
 function searchNote(search){
     var note = document.getElementsByClassName('note');
     filter = search.value.toUpperCase();
-    console.log(note)
-
     for (i = 0; i < note.length; i++) {
-        a = note[i].getElementsByTagName("p")[0];
-        txtValue = a.textContent || a.innerText;
+        text = note[i].getElementsByTagName("p")[0];
+        title = note[i].getElementsByTagName("h1")[0];
+        txtValue = text.textContent + ' '+ title.textContent ;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
             note[i].style.display = "";
         } else {
